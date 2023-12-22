7585a930d336e625df3a68fdd1c47fce5a845cb2
GSVolpato91 <guido.svolpato@gmail.com>
2023-12-23 01:10:25 +0900

    Kadai Thymeleaf first commit

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -1,5 +1,16 @@
 package com.techacademy;
 
-public class OutputController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class OutputController{
+
+  @PostMapping("/output")
+  public String postOutput(@RequestParam(name = "previous", required = false) String previous, Model model) {
+      model.addAttribute("previous", previous);
+      return "output";
+  }
 }
